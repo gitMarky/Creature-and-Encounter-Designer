bc34c2d4ad64ab306d7f8cbf38c148160e9944ea
Mark <markh4b@hotmail.com>
2016-06-04 13:04:09 +0200

    Html export can print a creature now

--- src/project/thirteenthage/creatures/internal/HtmlExporter.java
+++ src/project/thirteenthage/creatures/internal/HtmlExporter.java
@@ -7,8 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import project.thirteenthage.creatures.internal.conversions.Conversions;
 import project.thirteenthage.creatures.internal.gui.views.CreatureEncounterPanel;
+import project.thirteenthage.creatures.internal.interfaces.IAttack;
 import project.thirteenthage.creatures.internal.interfaces.ICreature;
+import project.thirteenthage.creatures.internal.interfaces.ISpecial;
 
 
 /**
@@ -68,11 +71,12 @@ public class HtmlExporter
 	
 	private void parseCreature(ICreature creature, int amount, int tabDepth)
 	{
+		content.append(tab(tabDepth) + "<h4 style=\"" + styleBackgrounDark() + ";" + styleFontWhite() + ";margin-top:30px;margin-bottom:3px;padding:5px\">" + creature.getName() + "</h4>" + Constants.NEWLINE);
 		content.append(tab(tabDepth) + Html.BEGIN_DIV);
-		content.append(tab(tabDepth) + "<table style=\"border-color:rgb(136,136,136);border-width:1px;border-collapse:collapse;width:100%\" border=\"1\" bordercolor=\"#888888\" cellpadding=\"7\" cellspacing=\"0\">" + Constants.NEWLINE);
+		content.append(tab(tabDepth) + "<table style=\"" + "border-color:rgb(136,136,136);border-width:1px;border-collapse:collapse;width:100%" + "\" border=\"1\" bordercolor=\"#888888\" cellpadding=\"7\" cellspacing=\"0\">" + Constants.NEWLINE);
 		content.append(tab(tabDepth) + "<thead>" + Constants.NEWLINE);
 		content.append(tab(tabDepth + 1) + "<tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 2) + "<td style=\"text-align:center;width:10%;background-color:rgb(100,150,255)\">" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 2) + "<td style=\"" + styleSidebar() + ";" + styleBackgroundLight() + "\">" + Constants.NEWLINE);
 
 		parseCreatureInfo(creature, tabDepth + 3);
 		
@@ -82,7 +86,7 @@ public class HtmlExporter
 		parseCreatureAttacks(creature, tabDepth + 3);
 		
 		content.append(tab(tabDepth + 2) + "</td>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 2) + "<td style=\"text-align:center;width:10%;background-color:rgb(100,150,255)\">" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 2) + "<td style=\"" + styleSidebar() + ";" + styleBackgroundLight() + "\">" + Constants.NEWLINE);
 
 		parseCreatureStats(creature, tabDepth + 3);
 		
@@ -96,40 +100,80 @@ public class HtmlExporter
 		content.append(tab(tabDepth) + Html.END_DIV);
 	}
 
+
+	private String styleBackgrounDark()
+	{
+		return "background-color:rgb(0,51,153)";
+	}
+
+
+	private String styleSidebar()
+	{
+		return "text-align:center;width:10%";
+	}
+
+
+	private String styleBackgroundLight()
+	{
+		return "background-color:rgb(100,150,255)";
+	}
+
 	
 	private void parseCreatureInfo(ICreature creature, int tabDepth)
 	{
-		content.append(tab(tabDepth) + "<table style=\"color:rgb(255,255,255)\">" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>Normal</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>2nd level</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>Wrecker</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>Devil</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>Baatezu</td></tr>" + Constants.NEWLINE);
+		content.append(tab(tabDepth) + "<table style=\"" + styleFontWhite() + "\">" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 1) + "<tr><td>" + creature.getSize().toGuiText() + "</td></tr>" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 1) + "<tr><td>" + LevelHelper.getLevelText(creature.getLevel()) + "</td></tr>" + Constants.NEWLINE);
+		
+		for (final String label : creature.getLabels())
+		{
+			content.append(tab(tabDepth + 1) + "<tr><td>" + label + "</td></tr>" + Constants.NEWLINE);			
+		}
+		
 		content.append(tab(tabDepth) + "</table>" + Constants.NEWLINE);
 	}
 
 	
 	private void parseCreatureAttacks(ICreature creature, int tabDepth)
 	{
-		content.append(tab(tabDepth) + "<p><b>Initiative +6</b></p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><b>Furious claws and fangs</b> +7 AC / 6 damage<br><i>Natural 11+</i>: The abishai can make another melee attack as a free action, but no more than three per round.</p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><b>Poison Tail</b>+7 AC / 5 damage, and 5 ongoing acid damage. Once per battle.</p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><b>Dive</b> +9 AC / 8 damage</p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><i>Flight</i></p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><i>Baatezu Regeneration</i>: The abishai regenerates 1 hp per level at the start of each round, up to 3 times. An attack with holy water removes one regeneration.</p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><u>Nastier Specials</u></p>" + Constants.NEWLINE);
-		content.append(tab(tabDepth) + "<p><i>Scaly Hide</i>: The abishai has resist damage 12+ against attacks targeting AC.</p>" + Constants.NEWLINE);
+		content.append(tab(tabDepth) + "<p><b>Initiative " + String.format("%+d", creature.getInitiative()) + "</b></p>" + Constants.NEWLINE);
+		
+		for (final IAttack attack : creature.getAttacks())
+		{
+			content.append(tab(tabDepth) + Html.BEGIN_PARAGRAPH + attack.toGuiText() + Html.END_PARAGRAPH + Constants.NEWLINE);
+		}
+
+		for (final ISpecial special : creature.getSpecials())
+		{
+			content.append(tab(tabDepth) + Html.BEGIN_PARAGRAPH + special.toGuiText() + Html.END_PARAGRAPH + Constants.NEWLINE);
+		}
+
+		if (!creature.getNastierSpecials().isEmpty())
+		{
+			content.append(tab(tabDepth) + "<p><u>Nastier Specials</u></p>" + Constants.NEWLINE);
+		}
+
+		for (final ISpecial special : creature.getNastierSpecials())
+		{
+			content.append(tab(tabDepth) + Html.BEGIN_PARAGRAPH + special.toGuiText() + Html.END_PARAGRAPH + Constants.NEWLINE);
+		}
 	}
 	
 	
 	private void parseCreatureStats(ICreature creature, int tabDepth)
 	{
-		content.append(tab(tabDepth) + "<table style=\"color:rgb(255,255,255)\">" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>AC</td><td>18</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>PD</td><td>16</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>MD</td><td>12</td></tr>" + Constants.NEWLINE);
-		content.append(tab(tabDepth + 1) + "<tr><td>HP</td><td>36</td></tr>" + Constants.NEWLINE);
+		content.append(tab(tabDepth) + "<table style=\"" + styleFontWhite() + "\">" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 1) + "<tr><td>AC</td><td>" + creature.getAC() + "</td></tr>" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 1) + "<tr><td>PD</td><td>" + creature.getPD() + "</td></tr>" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 1) + "<tr><td>MD</td><td>" + creature.getMD() + "</td></tr>" + Constants.NEWLINE);
+		content.append(tab(tabDepth + 1) + "<tr><td>HP</td><td>" + Conversions.round(creature.getHP()) + "</td></tr>" + Constants.NEWLINE);
 		content.append(tab(tabDepth) + "</table>" + Constants.NEWLINE);
+	}
+
+
+	private String styleFontWhite()
+	{
+		return "color:rgb(255,255,255)";
 	}
 
 
