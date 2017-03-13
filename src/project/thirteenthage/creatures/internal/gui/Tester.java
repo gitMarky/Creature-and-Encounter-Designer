8d238faca8e0c3cae1c0efd8f230182d4106cf4f
Mark <markh4b@hotmail.com>
2017-03-13 18:25:19 +0100

    Simplify some stuff

--- src/project/thirteenthage/creatures/internal/gui/Tester.java
+++ src/project/thirteenthage/creatures/internal/gui/Tester.java
@@ -1,20 +1,17 @@
 package project.thirteenthage.creatures.internal.gui;
 
-import java.io.File;
-
 import javax.swing.JFrame;
 
-import project.thirteenthage.creatures.creature.CreatureTemplate;
 import project.thirteenthage.creatures.internal.gui.views.CreatureViewPanel;
 import project.thirteenthage.creatures.internal.interfaces.ICreature;
+import project.thirteenthage.creatures.internal.interfaces.ICreatureTemplate;
+import project.thirteenthage.creatures.loaders.CreatureTemplateLoader;
 
 public class Tester
 {
 	public static void main(final String[] args)
 	{
-		final File xml = new File("resources/creatures/creature_dire_rat.xml");
-
-		final CreatureTemplate template = new CreatureTemplate(xml, "creature_dire_rat");
+		final ICreatureTemplate template = CreatureTemplateLoader.getInstance().get("creature_dire_rat");
 		final ICreature rat = template.toCreature();
 
 		final JFrame frame = new JFrame("Test viewer");
